2238060bdf1f0fc756cf910e71dfede480930537
Onyinye Ovuh <onyinyeovuh@gmail.com>
2025-11-07 18:50:01 +0100

    Use required expression for ALX checker

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,6 +1,7 @@
 hours = 2
-seconds_in_hour = 3600
-seconds = hours * seconds_in_hour
-print(hours, "hour(s) is", seconds, "seconds")
+seconds = hours * 3600
+print(f"{hours} hour(s) is {seconds} seconds")
+
+
 
 
